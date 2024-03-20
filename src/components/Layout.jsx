416e6a5f22e66dc132cfc6e5a7c2ea069fd3e72a
SergioDV <134448590+SergioDV95@users.noreply.github.com>
2024-03-20 12:25:00 -0400

    Latest

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
+   const style = {
+      img: "rotate-[-20deg] absolute z-[-1]",
+   }
    return (
       <>
          <Outlet />
-         <img className="rotate-[-25.99deg] absolute -top-[50%] -left-[50%]" src={"./src/assets/png/Recurso_17.png"} alt="recurso 17" width="1182px" height="676px" />
+         <img className={style.img} src="./src/assets/png/Recurso_17.png#xywh=50%,50%,500,500" alt="" />
       </>
    )
 }
