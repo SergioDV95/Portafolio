cab022a69108196c0625f99b1ceac20c554e9ed6
SergioDV <134448590+SergioDV95@users.noreply.github.com>
2024-03-26 23:24:29 -0400

    Latest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,17 +18,19 @@ export default function Home() {
 
    useEffect(() => {
       if (lang === "es") {
-         setText(text => ({...text, aboutMe: [
+         const aboutMe = [
             "Desarrollador Web Full Stack Jr., cuento con experiencia en el desarrollo de aplicaciones web dinámicas y responsivas.",
             "Me apasiona el aprendizaje continuo y la mejora de mis habilidades técnicas y creativas. Tengo capacidad para trabajar en equipo, resolver problemas y adaptarme a diferentes requisitos y tecnologías.",
             "Mi objetivo es crear soluciones web innovadoras y de calidad que satisfagan las necesidades de los usuarios y los clientes.",
-         ]}));
+         ]
+         setText({ aboutMe: aboutMe});
       } else {
-         setText(text => ({...text, aboutMe: [
-            "Full Stack Web Developer Jr., I have experience in developing dynamic and responsive web applications.",
+         const aboutMe = [
+            "Full Stack Jr. Web Developer, I have experience in developing dynamic and responsive web applications.",
             "I am passionate about continuous learning and improving my technical and creative skills. I have the ability to work in a team, solve problems and adapt to different requirements and technologies.",
             "My goal is to create innovative and quality web solutions that meet the needs of users and clients.",
-         ]}));
+         ]
+         setText({ aboutMe: aboutMe});
       }
    }, [lang]);
 
@@ -55,28 +57,37 @@ export default function Home() {
                   "Ideas sculptor for the web, I shape and convert your most ambitious or simple projects into reality"
                }
             </h4>
-            <a className="font-dela bg-navbarDesktop p-[10px] rounded-[8px] border border-white appearance-none font-semibold"
+            <motion.a className="font-dela bg-navbarDesktop p-[10px] rounded-[8px] border border-white appearance-none font-semibold"
                href = {`src/assets/downloads/${lang === "es" ? "CV_Desarrollador.pdf" : "CV_Developer.pdf"}`}
                download
+               whileTap={{ scale: 0.9 }}
                type="application/pdf"
             >
                <h4 className="">
                   {lang === "es" ? "DESCARGAR CV" : "DOWNLOAD CV"}
                </h4>
-            </a>
+            </motion.a>
          </div>
          <div className="flex max-lg:flex-col gap-[30px] max-lg:items-center">
             <figure className="relative mt-[15%] w-[175px] h-[175px] rounded-full bg-picture overflow-visible">
                <img className="scale-[1.20] z-50 -rotate-[2deg] absolute bottom-[10%] " src="./src/assets/png/sergio-daza.png" alt="Yo"/>
                <img className="scale-[2]" src={lang === "es" ? "./src/assets/png/ES/dark/sobre_mi.png" : "./src/assets/png/EN/dark/about_me.png"} alt="" />
             </figure>
-            <h1 className="font-dela max-lg:text-center text-[24px]">
+            <h1 className="font-dela max-lg:text-center text-[24px] text-[#FFBE00]">
                {lang === "es" ? "SOBRE MÍ" : "ABOUT ME"}
             </h1>
             <div className="bg-[#FFBE00] w-[15%] h-[2px] rounded-full"></div>
             <div className="text-center leading-[22px] px-[5%] ">
                {textSelector()}
             </div>
+         </div>
+         <div className="flex relative max-lg:flex-col gap-[30px] max-lg:items-center">
+            <img className="absolute -left-[45%] scale-[0.45] -top-[8%] " src="./src/assets/png/dark/flechas_der.png" alt="Right arrows"/>
+            <h1 className="font-dela max-lg:text-center text-[24px] text-[#FFBE00]">
+               {lang === "es" ? "HABILIDADES" : "SKILLS"}
+            </h1>
+            <div className="bg-[#FFBE00] w-[15%] h-[2px] rounded-full"></div>
+            <div></div>
          </div>
       </section>
    )
